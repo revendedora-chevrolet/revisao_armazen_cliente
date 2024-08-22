0c8d602962b4be48ed01d701e7ebffb3a40fc6ad
João Neto <j.altevirneto@gmail.com>
2024-08-22 09:17:19 -0300

    feat: feito processos de crud

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000
 const concessionariaController = require('./controllers/concessionaria.js');
+const estadoCarroController = require('./controllers/estadoCarro.js');
 
 app.use(express.json());
 
@@ -32,6 +33,34 @@ app.delete("concessionaria/:id", (req,res) => {
     concessionariaController.destroy(req.params.id);
     res.json();    
 });
+
+//GERENCIAMENTO ESTADO DE USO DO CARRO
+app.post("/estadoCarro", (req, res) => {
+    const estadoCarro = req.body;
+    const code = estadoCarroController.store(estadoCarro);
+    res.status(code).json();
+});
+
+app.get("/estadoCarro", (req, res) => {
+    res.json(estadoCarroController.index);
+});
+
+app.get("/estadoCarro/:id", (req,res) => {
+    const estadoCarro = estadoCarroController.show(req.params.id);
+    res.json(estadoCarro);
+});
+
+app.put("estadoCarro/:id", (req,res) => {
+    const estadoCarro = req.body;
+    const code = estadoCarroController.update(estadoCarro, req.params.id);
+    res.status(code).json();
+});
+
+app.delete("estadoCarro/:id", (req,res) => {
+    estadoCarroController.destroy(req.params.id);
+    res.json();    
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
