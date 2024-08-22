dd44bd8efb08aec54a5d57c4065e236aa5988014
João Neto <j.altevirneto@gmail.com>
2024-08-22 08:40:59 -0300

    init commit

---
+++ src/controllers/concessionaria.js
@@ -0,0 +1,60 @@
+const db = []
+let nextID = 1;
+
+const model = (body, id = nextID++) => {
+    if(
+        body.local != undefined,
+        body.local != "",
+        body.responsavel != undefined,
+        body.responsavel != "",
+        body.cnpj != undefined,
+        body.cnpj != ""
+    ){
+        return {
+            id,
+            local: body.local,
+            responsavel: body.responsavel,
+            cnpj: body.cnpj
+        }
+    }
+}
+
+const store = body => {
+    const novo = model(body);
+    if(novo){
+        db.push(novo);
+        return 201
+    }
+    return 400
+}
+
+const index = () => db;
+
+const show = id => db.find(el => el.id == id);
+
+const update = (body, id) => {
+    const index = db.findIndex(el => el.id == id);
+    const novo = model(body, parseInt(id));
+    if(novo && index != -1){
+        db[index] = novo;
+        return 200
+    }
+    return 400
+}
+
+const destroy = id => {
+    const index = db.findIndex(el => el.id == id);
+    if(index != -1){
+        db.splice(index,1);
+        return 200
+    }
+    return 400;
+}
+
+module.exports = {
+    store,
+    index,
+    show,
+    update,
+    destroy,
+}
